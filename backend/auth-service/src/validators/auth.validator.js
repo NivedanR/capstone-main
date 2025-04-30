e6346292b97de063d374d59bfdff2feee364f843
NivedanR <nivedanreddy2001@gmail.com>
2025-04-30 10:14:00 +0530

    auth-service-confirm-password-updated

--- backend/auth-service/src/validators/auth.validator.js
+++ backend/auth-service/src/validators/auth.validator.js
@@ -8,11 +8,15 @@ const validate = (schema) => (req, res, next) => {
 };
 
 exports.register = validate(Joi.object({
-  username: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  role: Joi.string().valid(...Object.values(ROLES))
-}));
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.any().valid(Joi.ref('password')).required().messages({
+      'any.only': 'Passwords do not match'
+    }),
+    role: Joi.string().valid(...Object.values(ROLES))
+  }));
+  
 
 exports.login = validate(Joi.object({
   email: Joi.string().email().required(),
